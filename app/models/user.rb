8d164953a624e6a267a22b80cb90e5701f2d1d44
Daniel Trujillo <ganimedes@firemail.cc>
2019-05-10 20:33:48 +0200

    added data validations and first seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,33 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  MARITAL_STATUS = ['Single', 'Married', 'Divorced', 'Widowed']
+  EMPLOYMENT_STATUS = ['Employee', 'Informal Worker', 'Self-employed', 'Unemployed']
+  HOUSING_TYPE = ['Owner', 'Rental']
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :marital_status, presence: true, inclusion: { in: MARITAL_STATUS }
+  validates :birth_date, presence: true
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :mobile_phone, presence: true, numericality: true, uniqueness: true
+  validates :land_line, numericality: true, uniqueness: true
+  validates :id_number, presence: true, uniqueness: true
+  validates :issue_date, presence: true
+  validates :employment_status, presence: true, inclusion: { in: EMPLOYMENT_STATUS }
+  #validates :employer
+  validates :monthly_income, presence: true, numericality: true
+  validates :dependant_number, presence: true, numericality: true
+  validates :housing_type, presence: true, inclusion: { in: HOUSING_TYPE }
+  validates :housing_expenses, presence: true, numericality: true
+  validates :monthly_expenses, presence: true, numericality: true
+  validates :debts_number, presence: true, numericality: true
+  validates :total_debts_amount, presence: true, numericality: true
+  validates :debt_term, presence: true, numericality: true
+  validates :referal, presence: true
+  validates :id_photo, presence: true
+  validates :utility_bill, presence: true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+test_user = User.new(
+  first_name: 'Daniel',
+  last_name: 'Trujillo',
+  email: 'daniel@gmail.com',
+  password: '123456',
+  marital_status: 'Single',
+  birth_date: '1987-12-30',
+  city: 'Neiva',
+  address: 'Calle 56 1W-99',
+  mobile_phone: '3006747173',
+  land_line: '88763990',
+  id_number: '1029022045',
+  issue_date: '2004-01-15',
+  employment_status: 'Unemployed',
+  monthly_income: 2000000,
+  dependant_number: 0,
+  housing_type: 'Owner',
+  housing_expenses: 500000,
+  monthly_expenses: 400000,
+  debts_number: 1,
+  total_debts_amount: 200000,
+  debt_term: 1,
+  referal: '123xyz',
+  id_photo: 'xxx',
+  utility_bill: 'yyy')
+
+test_user.save!
